6bb4793a565307e599a76626165923228dbb3bbd
Ryangregoire286-star <ryangregoire286@gmail.com>
2026-03-26 14:35:59 -0400

    updated to gcc 15

--- CPP_17/Source.cpp
+++ CPP_17/Source.cpp
@@ -9,26 +9,37 @@ namespace Color {
 
 	static auto GetColorsCode(int a, int b, int c) {
 
-		auto [x, y, z] = std::make_tuple(a, b, c);
-		std::cout << "R: " << x << "\n";
-		std::cout << "G: " << y << "\n";
-		std::cout << "B: " << z << "\n";
+		if constexpr (sizeof(a) > 0) {
+			auto [x, y, z] = std::make_tuple(a, b, c);
+			std::cout << "R: " << x << "\n";
+			std::cout << "G: " << y << "\n";
+			std::cout << "B: " << z << "\n";
+		}
 	}
 
 	static void HexColor(int r, int g, int b) {
 
-		std::cout << endl;
-		auto [x, y, z] = std::make_tuple(r, g, b);
-		std::cout << "Hex: " << std::hex << x << std::hex << y << std::hex << z << std::endl;
+		if constexpr (sizeof(r) > 0) {
+
+			std::cout << endl;
+			auto [x, y, z] = std::make_tuple(r, g, b);
+			std::cout << "Hex: " << std::hex << x << std::hex << y << std::hex << z << std::endl;
+
+		}
 
 	}
+
 
 	static int random_rgb(int color) {
 
+		if constexpr (sizeof(color) > 0) {
+			return rand() % color + 1;
+		}
+		
 		return rand() % color + 1;
 	}
-}
 
+}
 
 int Red = Color::random_rgb(254);
 int Green = Color::random_rgb(254);
@@ -37,16 +48,20 @@ int Blue = Color::random_rgb(254);
 bool isColorEnabled = false;
 
 int main() {
-	
+
+
 	while (!isColorEnabled) {
-		
-		Color::GetColorsCode(Red, Green, Blue);
-		Color::HexColor(Red, Green, Blue);
-		
-		isColorEnabled = true;
 
+		if constexpr (sizeof(int) > 0) {
+
+			Color::GetColorsCode(Red, Green, Blue);
+			Color::HexColor(Red, Green, Blue);
+
+			isColorEnabled = true;
+
+		}
+
+		system("pause > 0");
+		return 0;
 	}
-
-	system("pause > 0");
-	return 0;
 }
